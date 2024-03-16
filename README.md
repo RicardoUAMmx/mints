6fbe4c7f559339aea3366e932f83233c83a950f9
Ricardo Hernández Osorio <2172040687@correo.ler.uam.mx>
2024-03-16 03:47:49 -0600

    From-To Function or kind of

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
+# The Process
 
 ## TODO List
 
-- Create a function to build a 2d array dinamically
-- Create a function to free the memory of a 2d array
-- Create a function to print a 2d array
-- Create a function to fill a 2d array with a sequence of numbers
-- Create a function to fill a 2d array with ones
-- Create a function to fill a 2d array with zeros
-- Create a function to fill a 2d array with random numbers
-- Create a function to fill with an identity matrix
+1. Create a function to build a 2d array dinamically
+2. Create a function to free the memory of a 2d array
+3. Create a function to print a 2d array
+4. Create a function to fill a 2d array with a sequence of numbers
+5. Create a function to fill a 2d array with ones
+6. Create a function to fill a 2d array with zeros
+7. Create a function to fill a 2d array with random numbers
+8. Create a function to fill with an identity matrix
+9. Create a function with a copy of a 2d array from x,y and n size
+
+## Modifications
+
+1. `int** m_create( int x, int y )` - Create a 2d array with x and y size
+2. `void m_free( int** m, int x )` - Free the memory of a 2d array
+3. `void m_print( int** m, int x, int y )` - Print a 2d array
--- main.c
+++ main.c
@@ -1,107 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** m_create( int size );
-void m_free( int** mints, int size );
-void m_print( int** mints, int size );
-void m_fill_sequence( int** mints, int size, int start );
-void m_fill_random( int** mints, int size, int min, int max );
-void m_fill_zero( int** mints, int size );
-void m_fill_identity( int** mints, int size );
-int m_size( int** mints );
-int** m_copy( int** mints, int size, int fromx, int fromy );
-void m_fill_from( int** mints, int size, int fromx, int fromy, int** mcopy, int size2 );
+int** m_create( int x, int y );
+void m_free( int** mints, int x );
+void m_print( int** mints, int x, int y );
+void m_fill_sequence( int** mints, int x, int y, int start );
+void m_fill_random( int** mints, int x, int y, int min, int max );
+void m_fill_zero( int** mints, int x, int y );
+void m_fill_identity( int** mints, int x, int y );
+int** m_get( int** mints, int fromx, int fromy, int tox, int toy );
+// void m_fill_from( int** mints, int size, int fromx, int fromy, int** mcopy, int size2 );
 
 int main( void ){
-	int size = 32;
-	int** mints = m_create( size );
-	m_fill_sequence( mints, size, 1 );
-	m_print( mints, size );
+	int x = 16, y = 8;
+	int** ma = m_create( x, y );
+	m_fill_sequence( ma, x, y, 1 );
+	m_print( ma, x, y );
+	int** mb = m_get( ma, 1, 1, 6, 5 );
 	printf( "\n" );
 	printf( "\n" );
-	int size2 = 16;
-	int** mcopy = m_create( size2 );
-	m_fill_zero( mcopy, size2 );
-	m_print( mcopy, size2 );
-	printf( "\n" );
-	printf( "\n" );
-	m_fill_from( mints, size, 4, 4, mcopy, size2 );
-	m_print( mints, size2 );
-	m_free( mints, size );
-	m_free( mcopy, size2 );
+	m_print( mb, 5, 4 );
+	m_free( ma, y );
+	m_free( mb, 4 );
 	return 0;
 }
 
-int** m_create( int size ){
-	int** mints = (int**)malloc( size * sizeof(int*) );
-	for( int i=0; i<size; i++ ){
-		mints[i] = (int*)malloc( size * sizeof(int) );
+int** m_create( int x, int y ){
+	int** mints = (int**)malloc( y * sizeof(int*) );
+	for( int i=0; i<y; i++ ){
+		mints[i] = (int*)malloc( x * sizeof(int) );
 	}
 	return mints;
 }
 
-void m_free( int** mints, int size ){
-	for( int i=0; i<size; i++ ){
+void m_free( int** mints, int y ){
+	for( int i=0; i<y; i++ ){
 		free( mints[i] );
 	}
 	free( mints );
 }
 
-void m_print( int** mints, int size ){
-	for( int i=0; i<size; i++ ){
-		for( int j=0; j<size; j++ ){
+void m_print( int** mints, int x, int y ){
+	for( int i=0; i<y; i++ ){
+		for( int j=0; j<x; j++ ){
 			printf( "%-5d ", mints[i][j] );
 		}
 		printf( "\n" );
 	}
 }
 
-void m_fill_sequence( int** mints, int size, int start ){
-	for( int i=0; i<size; i++ ){
-		for( int j=0; j<size; j++ ){
+void m_fill_sequence( int** mints, int x, int y, int start ){
+	for( int i=0; i<y; i++ ){
+		for( int j=0; j<x; j++ ){
 			mints[i][j] = start++;
 		}
 	}
 }
 
-void m_fill_random( int** mints, int size, int min, int max ){
-	for( int i=0; i<size; i++ ){
-		for( int j=0; j<size; j++ ){
+void m_fill_random( int** mints, int x, int y, int min, int max ){
+	for( int i=0; i<y; i++ ){
+		for( int j=0; j<x; j++ ){
 			mints[i][j] = rand() % (max-min+1) + min;
 		}
 	}
 }
 
-void m_fill_zero( int** mints, int size ){
-	for( int i=0; i<size; i++ ){
-		for( int j=0; j<size; j++ ){
+void m_fill_zero( int** mints, int x, int y ){
+	for( int i=0; i<y; i++ ){
+		for( int j=0; j<x; j++ ){
 			mints[i][j] = 0;
 		}
 	}
 }
 
-void m_fill_identity( int** mints, int size ){
-	for( int i=0; i<size; i++ ){
-		for( int j=0; j<size; j++ ){
+void m_fill_identity( int** mints, int x, int y ){
+	for( int i=0; i<y; i++ ){
+		for( int j=0; j<x; j++ ){
 			mints[i][j] = (i==j) ? 1 : 0;
 		}
 	}
 }
 
-int** m_copy( int** mints, int size, int fromx, int fromy ){
-	int** mcopy = m_create( size );
-	for( int i=fromx; i<fromx+size; i++ ){
-		for( int j=fromy; j<fromy+size; j++ ){
-			mcopy[i-fromx][j-fromy] = mints[i][j];
+int** m_get( int** mints, int fromx, int fromy, int tox, int toy ){
+	int x = tox-fromx;
+	int y = toy-fromy;
+	int** mcopy = m_create( x, y );
+	for( int i=fromy; i<toy; i++ ){
+		for( int j=fromx; j<tox; j++ ){
+			mcopy[i-fromy][j-fromx] = mints[i][j];
 		}
 	}
 	return mcopy;
 }
 
-void m_fill_from( int** mints, int size, int fromx, int fromy, int** mcopy, int size2 ){
-	for( int i=fromx; i<fromx+size2; i++ ){
-		for( int j=fromy; j<fromy+size2; j++ ){
-			mints[i][j] = mcopy[i-fromx][j-fromy];
-		}
-	}
-}
+// void m_fill_from( int** mints, int size, int fromx, int fromy, int** mcopy, int size2 ){
+// 	for( int i=fromx; i<fromx+size2; i++ ){
+// 		for( int j=fromy; j<fromy+size2; j++ ){
+// 			mints[i][j] = mcopy[i-fromx][j-fromy];
+// 		}
+// 	}
+// }
