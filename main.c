fa5a38c81ec7bc49783a06adddbf00fca66b419e
Ricardo Hernández Osorio <2172040687@correo.ler.uam.mx>
2024-03-17 03:22:31 -0600

    Fix the put_identity in accordance with put functions

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** m_create( int x, int y );
-void m_free( int** mints, int x );
-void m_print( int** mints, int fromx, int fromy, int tox, int toy);
-void m_fill_sequence( int** mints, int x, int y, int start, int step );
-void m_fill_random( int** mints, int x, int y, int min, int max );
-void m_fill_identity( int** mints, int x, int y );
-int** m_get( int** mints, int fromx, int fromy, int tox, int toy );
-int** m_init( int shape_x, int shape_y, int param_1, int param_2, void (*fill)( int**, int, int, int, int ) );
+int** mint_alloc( int x, int y );
+void mint_free( int** mints, int x );
+void mint_print( int** mints, int fromx, int fromy, int tox, int toy);
+void mint_put_seq( int** mints, int fromx, int fromy, int tox, int toy, int start, int step );
+void mint_put_ran( int** mints, int fromx, int fromy, int tox, int toy, int min, int max );
+void mint_put_identity( int** mints, int x, int y );
+int** mint_get( int** mints, int fromx, int fromy, int tox, int toy );
+void mint_put( int** mints, int fromx, int fromy, int tox, int toy, int param_1, int param_2, void (*put)( int**, int, int, int, int, int, int ) );
 
 int main( void ){
-	int x = 16, y = 8;
-	int** ma = m_init( x, y, 0, 1, m_fill_sequence );
-	m_print( ma, 0, 0, x, y );
-	int** mb = m_get( ma, 1, 1, 6, 5 );
+
+	int xa = 16, ya = 8;
+	int** ma = mint_alloc( xa, ya );
+	mint_put( ma, 0, 0, xa, ya, 1, 1, mint_put_seq );
+
+	mint_print( ma, 0, 0, xa, ya );
 	printf( "\n" );
 	printf( "\n" );
-	m_print( mb, 0, 0, 5, 4 );
-	m_free( ma, y );
-	m_free( mb, 4 );
+
+	int xb = 5, yb = 4;
+	int** mb = mint_get( ma, 1, 1, 6, 5 );
+
+	mint_print( mb, 0, 0, 5, 4 );
+	mint_free( ma, ya );
+	mint_free( mb, yb );
 	return 0;
 }
 
-int** m_create( int x, int y ){
+int** mint_alloc( int x, int y ){
 	int** mints = (int**)malloc( y * sizeof(int*) );
 	for( int i=0; i<y; i++ ){
 		mints[i] = (int*)malloc( x * sizeof(int) );
@@ -31,14 +37,14 @@ int** m_create( int x, int y ){
 	return mints;
 }
 
-void m_free( int** mints, int y ){
+void mint_free( int** mints, int y ){
 	for( int i=0; i<y; i++ ){
 		free( mints[i] );
 	}
 	free( mints );
 }
 
-void m_print( int** mints, int fromx, int fromy, int tox, int toy ){
+void mint_print( int** mints, int fromx, int fromy, int tox, int toy ){
 	for( int i=fromy; i<toy; i++ ){
 		for( int j=fromx; j<tox; j++ ){
 			printf( "%-5d ", mints[i][j] );
@@ -47,23 +53,23 @@ void m_print( int** mints, int fromx, int fromy, int tox, int toy ){
 	}
 }
 
-void m_fill_sequence( int** mints, int x, int y, int start, int step ){
-	for( int i=0; i<y; i++ ){
-		for( int j=0; j<x; j++ ){
-			mints[i][j] = start + j*step + i*x*step;
+void mint_put_seq( int** mints, int fromx, int fromy, int tox, int toy, int start, int step ){
+	for( int i=fromy; i<toy; i++ ){
+		for( int j=fromx; j<tox; j++ ){
+			mints[i][j] = start + (i-fromy)*tox + (j-fromx)*step;
 		}
 	}
 }
 
-void m_fill_random( int** mints, int x, int y, int min, int max ){
-	for( int i=0; i<y; i++ ){
-		for( int j=0; j<x; j++ ){
+void mint_put_ran( int** mints, int fromx, int fromy, int tox, int toy, int min, int max ){
+	for( int i=fromy; i<toy; i++ ){
+		for( int j=fromx; j<tox; j++ ){
 			mints[i][j] = rand() % (max-min+1) + min;
 		}
 	}
 }
 
-void m_fill_identity( int** mints, int x, int y ){
+void mint_put_identity( int** mints, int x, int y ){
 	for( int i=0; i<y; i++ ){
 		for( int j=0; j<x; j++ ){
 			mints[i][j] = (i==j) ? 1 : 0;
@@ -71,10 +77,10 @@ void m_fill_identity( int** mints, int x, int y ){
 	}
 }
 
-int** m_get( int** mints, int fromx, int fromy, int tox, int toy ){
+int** mint_get( int** mints, int fromx, int fromy, int tox, int toy ){
 	int x = tox-fromx;
 	int y = toy-fromy;
-	int** mcopy = m_create( x, y );
+	int** mcopy = mint_alloc( x, y );
 	for( int i=fromy; i<toy; i++ ){
 		for( int j=fromx; j<tox; j++ ){
 			mcopy[i-fromy][j-fromx] = mints[i][j];
@@ -83,9 +89,7 @@ int** m_get( int** mints, int fromx, int fromy, int tox, int toy ){
 	return mcopy;
 }
 
-int** m_init( int shape_x, int shape_y, int param_1, int param_2, void (*fill)( int**, int, int, int, int ) ){
-	int** mints = m_create( shape_x, shape_y );
-	fill( mints, shape_x, shape_y, param_1, param_2 );
-	return mints;
+void mint_put( int** mints, int fromx, int fromy, int tox, int toy, int param_1, int param_2, void (*put)( int**, int, int, int, int, int, int ) ){
+	put( mints, fromx, fromy, tox, toy, param_1, param_2 );
 }
 
