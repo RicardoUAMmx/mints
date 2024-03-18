6d348ccb1045982e86cfa08eabe01dc7906f4ab8
Ricardo Hernández Osorio <2172040687@correo.ler.uam.mx>
2024-03-18 00:55:11 -0600

    get_rot enhanced to perform just in certain zone of mints

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ void mint_free( int** mints, int x );
 void mint_print( int** mints, int fromx, int fromy, int tox, int toy);
 void mint_put_seq( int** mints, int fromx, int fromy, int tox, int toy, int start, int step );
 void mint_put_ran( int** mints, int fromx, int fromy, int tox, int toy, int min, int max );
-void mint_put_identity( int** mints, int x, int y );
-int** mint_get( int** mints, int fromx, int fromy, int tox, int toy );
+void mint_get( int** a_mints, int a_fromx, int a_fromy, int a_tox, int a_toy, int** b_mints, int b_fromx, int b_fromy, int b_tox, int b_toy );
+void mint_get_rot( int** mints, int x, int y );
 void mint_put( int** mints, int fromx, int fromy, int tox, int toy, int param_1, int param_2, void (*put)( int**, int, int, int, int, int, int ) );
 
 int main( void ){
@@ -20,17 +20,16 @@ int main( void ){
 	printf( "\n" );
 	printf( "\n" );
 
-	int xb = 5, yb = 4;
-	int** mb = mint_get( ma, 1, 1, 6, 5 );
-
-	mint_print( mb, 0, 0, xb, yb );
+	mint_get_rot( ma, xa, ya );
+	
+	mint_print( ma, 0, 0, xa, ya );
 	printf( "\n" );
 	printf( "\n" );
 
 	mint_free( ma, ya );
-	mint_free( mb, yb );
 
 	return 0;
+
 }
 
 int** mint_alloc( int x, int y ){
@@ -73,24 +72,31 @@ void mint_put_ran( int** mints, int fromx, int fromy, int tox, int toy, int min,
 	}
 }
 
-void mint_put_identity( int** mints, int x, int y ){
-	for( int i=0; i<y; i++ ){
-		for( int j=0; j<x; j++ ){
-			mints[i][j] = (i==j) ? 1 : 0;
+// From a mints to another mints b.
+void mint_get( int** a_mints, int a_fromx, int a_fromy, int a_tox, int a_toy, int** b_mints, int b_fromx, int b_fromy, int b_tox, int b_toy ){
+	for( int i=a_fromy; i<a_toy; i++ ){
+		for( int j=a_fromx; j<a_tox; j++ ){
+			b_mints[i-a_fromy+b_fromy][j-a_fromx+b_fromx] = a_mints[i][j];
 		}
 	}
 }
 
-int** mint_get( int** mints, int fromx, int fromy, int tox, int toy ){
-	int x = tox-fromx;
-	int y = toy-fromy;
-	int** mcopy = mint_alloc( x, y );
-	for( int i=fromy; i<toy; i++ ){
-		for( int j=fromx; j<tox; j++ ){
-			mcopy[i-fromy][j-fromx] = mints[i][j];
-		}
-	}
-	return mcopy;
+void mint_get_rot( int** mints, int x, int y ){
+	int xl = x/2, yl = y/2;
+	int** ma = mint_alloc( xl, yl );
+	int** mb = mint_alloc( xl, yl );
+
+	mint_get( mints, xl, 0, x, yl, ma, 0, 0, xl, yl );
+	mint_get( mints, xl, yl, x, y, mb, 0, 0, xl, yl );
+
+	mint_get( mints, 0, 0, xl, yl, mints, xl, 0, x, yl );
+	mint_get( ma, 0, 0, xl, yl, mints, xl, yl, x, y );
+	mint_get( mints, 0, yl, xl, y, ma, 0, 0, xl, yl );
+	mint_get( mb, 0, 0, xl, yl, mints, 0, yl, xl, y );
+	mint_get( ma, 0, 0, xl, yl, mints, 0, 0, xl, yl );
+
+	mint_free( ma, yl );
+	mint_free( mb, yl );
 }
 
 void mint_put( int** mints, int fromx, int fromy, int tox, int toy, int param_1, int param_2, void (*put)( int**, int, int, int, int, int, int ) ){
